100148c62b5057e0d61d33c72adbd25a100a33da
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2022-04-14 22:11:20 -0300

    Add Poppins global font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,42 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v19-latin-300.woff2') format('woff2'),
+       url('/fonts/poppins-v19-latin-300.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v19-latin-regular.woff2') format('woff2'),
+       url('/fonts/poppins-v19-latin-regular.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: local(''),
+       url('/fonts/poppins-v19-latin-600.woff2') format('woff2'),
+       url('/fonts/poppins-v19-latin-600.woff') format('woff');
+}
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
@@ -16,7 +48,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 `
 
